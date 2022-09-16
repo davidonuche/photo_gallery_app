e1f015554050f8064a6af83c7866592280ddc82d
David onuche <110064207+Davidbeebest@users.noreply.github.com>
2022-09-16 19:15:47 +0100

    first commit

---
+++ lib/screens/photo_gallery_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class PhotoGalleryScreen extends StatefulWidget {
+  const PhotoGalleryScreen
+({super.key});
+
+  @override
+  State<PhotoGalleryScreen> createState() => _PhotoGalleryScreenState();
+}
+
+class _PhotoGalleryScreenState extends State<PhotoGalleryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: GridView.builder(
+            itemCount: 6,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 6.0,
+              mainAxisSpacing: 6.0,
+               ), 
+               itemBuilder: (context, index) {
+                return Image.network("https://placeimg.com/640/480/any", fit: BoxFit.cover);
+               },
+               ),
+          ),
+      ),
+    );
+  }
+}
